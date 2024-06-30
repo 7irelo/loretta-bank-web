4103a32220feb46da2b61d88ba16072181976c05
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 15:03:51 +0200

    Update associations.js

--- server/models/associations.js
+++ server/models/associations.js
@@ -1,11 +1,21 @@
-const Account = require("./User");
+const User = require("./User");
 const Account = require("./Account");
 const Transaction = require("./Transaction");
+const Loan = require("./Loan");
+const CreditCard = require("./Credit");
 
-User.hasMany(Account, { foreignKey: "userId" });
-Account.belongsTo(User, { foreignKey: "userId" });
 
-Account.hasMany(Transaction, { foreignKey: "accountId" });
-Transaction.belongsTo(Account, { foreignKey: "accountId" });
+User.hasMany(Account, { foreignKey: 'userId' });
+Account.belongsTo(User, { foreignKey: 'userId' });
 
-module.exports = { User, Account, Transaction };
+User.hasMany(Transaction, { foreignKey: 'userId' });
+Transaction.belongsTo(User, { foreignKey: 'userId' });
+
+Account.hasMany(Transaction, { foreignKey: 'accountId' });
+Transaction.belongsTo(Account, { foreignKey: 'accountId' });
+
+User.hasMany(Loan, { foreignKey: 'userId' });
+Loan.belongsTo(User, { foreignKey: 'userId' });
+
+User.hasMany(CreditCard, { foreignKey: 'userId' });
+CreditCard.belongsTo(User, { foreignKey: 'userId' });
