68581439d2769e1c892943311a36d7cdbcc92721
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 18:13:28 +0200

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const verifyToken = require('../middlewares/verifyToken');
 
 const router = express.Router();
+router.use(verifyToken);
 
 router.post('/', userController.createUser);
 router.get('/:idNumber', userController.getUser);
