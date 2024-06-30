bbed1cf0c05a00caa3e7d951d9ecb83f63d10d25
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 18:09:19 +0200

    Update creditCardRoutes.js

--- server/routes/creditCardRoutes.js
+++ server/routes/creditCardRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const { createCreditCard, getCreditCard, updateCreditCard,  deleteCreditCard } = require('../controllers/creditCardController');
+const { 
+  createCreditCard, 
+  getCreditCard, 
+  updateCreditCard,
+  deleteCreditCard } = require('../controllers/creditCardController');
+const verifyToken = require('../middlewares/verifyToken');
 
 const router = express.Router();
+router.use(verifyToken);
 
 router.post('/', createCreditCard);
 router.get('/:id', getCreditCard);
