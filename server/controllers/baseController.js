fc404c0f9613da5b4ed015e23d3e7c4d8c3fb514
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 15:56:51 +0200

    Update baseController.js

--- server/controllers/baseController.js
+++ server/controllers/baseController.js
@@ -1,26 +1,12 @@
-const path = require("path");
-
-const indexPage = (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, "../views/index.html"));
+const getRoot = (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Bank API' });
 };
 
-const errorPage = async (req, res) {
-  res.const indexPage = (req, res) => {
-  res.status(200).json({ message: "Welcome to the index page" });
-};
-
-const errorPage = (req, res) => {
-  res.status(404).json({ error: "404 - Not Found" });
+const notFound = (req, res) => {
+  res.status(404).json({ error: 'Not Found' });
 };
 
 module.exports = {
-  indexPage,
-  errorPage,
-};
-(404).send("404 - Not Found");
-}
-
-module.exports = {
-  indexPage,
-  errorPage,
+  getRoot,
+  notFound,
 };
