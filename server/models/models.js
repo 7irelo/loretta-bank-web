bdd2840882fdce6b7cba92946efe6aaef234c0e7
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 09:28:54 +0200

    Update models.js

--- server/models/models.js
+++ server/models/models.js
@@ -1,48 +1,84 @@
 const dotenv = require("dotenv");
 const Sequelize = require("sequelize");
+const sequelize = require("../sequelize");
 
 dotenv.config();
-
-const sequelize = new Sequelize("lorettabank", "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-});
 
 const User = sequelize.define("User", {
   idNumber: {
     type: Sequelize.STRING,
     primaryKey: true,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+    },
   },
   firstName: {
     type: Sequelize.STRING(50),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
   },
   lastName: {
     type: Sequelize.STRING(50),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
   },
   address: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   dateOfBirth: {
     type: Sequelize.DATEONLY,
+    allowNull: false,
   },
   occupation: {
     type: Sequelize.STRING,
+    allowNull: true,
   },
   phone: {
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isNumeric: true,
+    },
   },
   email: {
     type: Sequelize.STRING,
+    allowNull: false,
     unique: true,
-    isEmail: true,
+    validate: {
+      isEmail: true,
+    },
   },
   username: {
     type: Sequelize.STRING,
+    allowNull: false,
     unique: true,
   },
   password: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
+  createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+}, {
+  indexes: [
+    {
+      unique: true,
+      fields: ['email', 'username', 'idNumber'],
+    },
+  ],
 });
 
 const Account = sequelize.define("Account", {
@@ -57,20 +93,54 @@ const Account = sequelize.define("Account", {
       model: User,
       key: "idNumber",
     },
+    allowNull: false,
   },
   accountType: {
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [['Savings', 'Checking', 'Credit']],
+    },
   },
   balance: {
     type: Sequelize.DOUBLE,
+    allowNull: false,
+    defaultValue: 0.0,
+    validate: {
+      min: 0,
+    },
   },
+  accountStatus: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'Active',
+    validate: {
+      isIn: [['Active', 'Inactive', 'Closed']],
+    },
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+}, {
+  indexes: [
+    {
+      fields: ['userId', 'accountType'],
+    },
+  ],
 });
 
-// User.hasMany(Account, {foriegnKey: "userId"})
-// Account.belongsTo(User, {foriegnKey: "userId"})
+User.hasMany(Account, { foreignKey: "userId" });
+Account.belongsTo(User, { foreignKey: "userId" });
 
-sequelize.sync().then(() => {
-  console.log("tables created");
+sequelize.sync({ alter: true }).then(() => {
+  console.log("Tables created and synchronized");
+}).catch((error) => {
+  console.error("Error creating tables:", error);
 });
 
 module.exports = { User, Account };
