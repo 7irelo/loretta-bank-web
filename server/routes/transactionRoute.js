e36903aded2d5e61ff0e5d5853f536d8c189ee1f
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 15:42:29 +0200

    Create transactionRoute.js

---
+++ server/routes/transactionRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const transactionController = require('../controllers/transactionController');
+
+const router = express.Router();
+
+router.post('/', transactionController.createTransaction);
+router.get('/:id', transactionController.getTransaction);
+router.put('/:id', transactionController.updateTransaction);
+router.delete('/:id', transactionController.deleteTransaction);
+
+module.exports = router;
