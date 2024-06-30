71b310c4164f346edc36227a167dcd41196a4972
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 18:18:31 +0200

    Update accountRoutes.js

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -1,23 +1,16 @@
 const express = require('express');
-const { 
-  getAccounts,
-  getAccount,
-  updateAccount,
-  deleteAccount,
-  queryAccounts,
-  getCPJTransactions,
-  getCRJTransactions } = require('../controllers/accountController');
+const accountController = require('../controllers/accountController');
 const verifyToken = require('../middlewares/verifyToken');
 
 const router = express.Router();
 router.use(verifyToken);
 
-router.get('/', getAccounts);
-router.get('/:accountID', getAccount);
-router.put('/:id', updateAccount);
-router.delete('/:id', deleteAccount);
-router.get('/query', queryAccounts);
-router.get('/transactions/cpj', getCPJTransactions);
-router.get('/transactions/crj', getCRJTransactions);
+router.get('/', accountController.getAccounts);
+router.get('/:accountID', accountController.getAccount);
+router.put('/:id', accountController.updateAccount);
+router.delete('/:id', accountController.deleteAccount);
+router.get('/query', accountController.queryAccounts);
+router.get('/transactions/cpj', accountController.getCPJTransactions);
+router.get('/transactions/crj', accountController.getCRJTransactions);
 
 module.exports = router;
