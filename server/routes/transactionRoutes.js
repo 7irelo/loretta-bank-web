5747e67259579e5d61b0869325a7f0965e3a861b
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 18:12:53 +0200

    Update transactionRoutes.js

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const transactionController = require('../controllers/transactionController');
+const verifyToken = require('../middlewares/verifyToken');
 
 const router = express.Router();
+router.use(verifyToken);
 
 router.post('/', transactionController.createTransaction);
 router.get('/:id', transactionController.getTransaction);
