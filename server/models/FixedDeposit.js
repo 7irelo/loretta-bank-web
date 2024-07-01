a39831064f9a2146d32bac6ef9ba23bf657cb529
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-01 06:55:22 +0200

    Update FixedDeposit.js

--- server/models/FixedDeposit.js
+++ server/models/FixedDeposit.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const User = require('./userModel');
+const User = require('./User');
 
 const FixedDeposit = sequelize.define('FixedDeposit', {
   id: {
