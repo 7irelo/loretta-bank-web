118c89b2ba6eb7c1d80dc353e76e2ad9f3a48603
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-17 16:34:03 +0200

    Update logger.js

--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -1,9 +1,13 @@
-function logger (req, res, next) {
+const logger = (req, res, next) => {
     const method = req.method;
     const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time);
+    const headers = JSON.stringify(req.headers);
+    const ip = req.ip || req.connection.remoteAddress;
+    const time = new Date().toISOString();
+    
+    console.log(`[${time}] ${method} ${url} - IP: ${ip} - Headers: ${headers}`);
+    
     next();
-}
+};
 
-module.exports = logger
+module.exports = logger;
