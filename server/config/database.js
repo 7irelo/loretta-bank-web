720876eec1dac8ca1d91457eb8fc74dacc8ab0c6
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-17 13:12:37 +0200

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,43 +1,14 @@
-const { Sequelize } = require('sequelize');
-const mysql = require('mysql2');
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 const dotenv = require('dotenv');
-const { database } = require('pg/lib/defaults');
 
 dotenv.config();
 
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "root",
-  password: "password",
-  database: "lorettaexpress"
+  host: process.env.PG_HOST || "localhost",
+  port: process.env.PG_PORT || 5432,
+  user: process.env.PG_USER || "root",
+  password: process.env.PG_PASSWORD || "password",
+  database: process.env.PG_DATABASE || "lorettaexpress"
 });
 
-
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  dialect: process.env.DATABASE_DIALECT,
-  operationsAliases: false,
-  logging: false,
-  pool : {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-}
-});
-
-// const pool = mysql.createPool({
-//   host: process.env.MYSQL_HOST,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DATABASE
-// }).promise();
-
-// async function getAll() {
-//   const [rows] = await pool.query("SELECT * FROM users")
-//   console.log(rows)
-// }
-// getAll();
-
-module.exports = { sequelize, pool };
+module.exports = { pool };
