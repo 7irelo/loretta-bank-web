340f599fe3fc11d73e0ea25c64b6dacfef01c909
7irelo <tirelo.eric@gmail.com>
2024-07-17 16:17:40 +0200

    Salt

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -6,11 +6,8 @@ const router = express.Router();
 // router.use(verifyToken);
 
 router.get('/', accountController.getAccounts);
-router.get('/:accountID', accountController.getAccount);
+router.get('/:id', accountController.getAccount);
 router.put('/:id', accountController.updateAccount);
 router.delete('/:id', accountController.deleteAccount);
-router.get('/query', accountController.queryAccounts);
-router.get('/transactions/cpj', accountController.getCPJTransactions);
-router.get('/transactions/crj', accountController.getCRJTransactions);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,18 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const logger = require("./middlewares/logger");
-const { sequelize } = require('./config/database');
 const authRoutes = require("./routes/authRoutes");
-const rootRoutes = require("./routes/rootRoutes");
-const userRoutes = require("./routes/userRoutes");
 const accountRoutes = require("./routes/accountRoutes");
-const creditCardRoutes = require("./routes/creditCardRoutes");
+const cardRoutes = require("./routes/cardRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 const loanRoutes = require("./routes/loanRoutes");
-const fixedDepositRoutes = require("./routes/fixedDepositRoutes");
 const pool = require("./config/database")
-
-console.log("Hello World")
 
 // Config
 dotenv.config();
@@ -27,13 +21,10 @@ app.use(logger);
 
 // Routes
 app.use('/auth', authRoutes);
-app.use('/', rootRoutes);
-app.use('/users', userRoutes);
 app.use('/accounts', accountRoutes);
-app.use('/credit-cards', creditCardRoutes);
+app.use('/cards', cardRoutes);
 app.use('/transactions', transactionRoutes);
 app.use('/loans', loanRoutes);
-app.use('/fixed-deposits', fixedDepositRoutes);
 
 app.get("/test", async (req, res) => {
   res.send({message: "hello"})
@@ -41,16 +32,7 @@ app.get("/test", async (req, res) => {
 
 // Initialize Sequelize and start server
 const PORT = process.env.PORT || 3000;
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-    return sequelize.sync({ alter: true });
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
