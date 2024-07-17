d444bb441dc256c490b697727972fd49978af90c
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-17 13:16:32 +0200

    Update Transaction.js

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,38 +1,20 @@
-const { DataTypes, Sequelize } = require('sequelize');
-const { sequelize } = require('../config/database');
-const Account = require('./Account');
+const { pool } = require('../config/database');
 
-const Transaction = sequelize.define('Transaction', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  accountId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Account,
-      key: 'id',
-    },
-  },
-  type: {
-    type: DataTypes.STRING, // 'credit' or 'debit'
-    allowNull: false,
-  },
-  amount: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  date: {
-    type: DataTypes.DATE,
-    defaultValue: Sequelize.NOW,
-  },
-  description: {
-    type: DataTypes.STRING,
-  },
-  journalType: {
-    type: DataTypes.STRING, // 'CPJ', 'CRJ', 'DJ', etc.
-  },
-});
+const createTransactionTable = async () => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS transactions (
+      id SERIAL PRIMARY KEY,
+      accountId INTEGER REFERENCES accounts(id),
+      type VARCHAR NOT NULL,
+      amount DOUBLE PRECISION NOT NULL,
+      date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      description VARCHAR,
+      journalType VARCHAR
+    );
+  `;
+  await pool.query(query);
+};
 
-module.exports = Transaction;
+createTransactionTable().catch(err => console.error('Error creating transaction table:', err));
+
+module.exports = { createTransactionTable };
